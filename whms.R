39cfc8830369bbf0a6ed4be814bd64bc3cd69972
Lennert Schepers <6145889+LennertSchepers@users.noreply.github.com>
2023-02-13 21:48:00 +0100

    initial commit

---
+++ whms.R
@@ -0,0 +1,8 @@
+library(sf)
+library(mapview)
+
+whms <- read_sf("https://geo.vliz.be/geoserver/MarineRegions/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=MarineRegions%3Aworldheritagemarineprogramme&maxFeatures=1000&outputFormat=application/json")
+mapview(whms)
+
+whms_df <- st_drop_geometry(whms)
+write.csv(whms_df, "whms.csv")
